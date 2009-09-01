9a1a2bfff2cd9ff38e6b9060758554958af11818
Glenn F. Henriksen <glenn@henriksen.no>
2009-09-01 12:58:30 +0200

    Setup NUnit

--- src/Mocking.Tests/T1.cs
+++ src/Mocking.Tests/T1.cs
@@ -1,7 +1,14 @@
+using NUnit.Framework;
+
 namespace Mocking.Tests
 {
+    [TestFixture]
     public class T1
     {
-        
+        [Test]
+        public void Example1()
+        {
+            
+        }
     }
 }
